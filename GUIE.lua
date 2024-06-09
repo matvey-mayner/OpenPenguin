92211bd6c27c7a0141949180929e7b46e7b04532
DanXvoIsMe <157852596+DanXvoIsMe@users.noreply.github.com>
2024-06-09 20:31:12 +0200

    Update GUIE.lua

--- GUIE.lua
+++ GUIE.lua
@@ -7,7 +7,7 @@ local event = require("event")
 local buttons = {}
 
 module.testButton = function(x, y)
-  for id,button in pairs(fgui.buttons) do
+  for id, button in pairs(buttons) do
       if x >= button.x1 and x <= button.x2 and y >= button.y1 and y <= button.y2 then
           return id
       end
@@ -26,9 +26,9 @@ function module.MakeButton(id, x, y, width, height, text, callback, foreground, 
   local textX = x + math.floor((width - #text) / 2)
   local textY = y + math.floor(height / 2)
   gpu.set(textX, textY, text)
-  buttons[id] = { x1 = x, y1 = y, x2 = width, y2 = height}
-  local buttonOnTouch = function(_, _, x, y, button, player)
-    if module.buttonTouch(x, y, id) then
+  buttons[id] = { x1 = x, y1 = y, x2 = x + width - 1, y2 = y + height - 1 }
+  local buttonOnTouch = function(_, _, touchX, touchY, button, player)
+    if module.buttonTouch(touchX, touchY, id) then
         callback(id, player, button)
     end
   end
