ad9db55995ff814b64d520b9bd75d0246b11a3cf
matvey-mayner <144268527+matvey-mayner@users.noreply.github.com>
2024-06-09 20:54:12 +0300

    Create GUIE.lua

---
+++ GUIE.lua
@@ -0,0 +1,10 @@
+local gpu = component.gpu
+
+local function MakeButton(x, y, width, height, text, foreground, background)
+  gpu.setForeground(foreground)
+  gpu.setBackground(background)
+  gpu.fill(x, y, width, height, " ")
+  local textX = x + math.floor((width - #text) / 2)
+  local textY = y + math.floor(height / 2)
+  gpu.set(textX, textY, text)
+end
