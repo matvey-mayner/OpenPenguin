507d254df4c4c8aa81152490b89b0d06a1fb4f8a
matvey-mayner <144268527+matvey-mayner@users.noreply.github.com>
2024-06-09 20:06:26 +0300

    Create autorun.lua

---
+++ autorun.lua
@@ -0,0 +1,41 @@
+local component = require("component")
+local event = require("event")
+local gpu = component.gpu
+
+gpu.setResolution(80,25)
+
+local function drawLoadingBar()
+  local barWidth = 50
+  local barHeight = 1
+  local barX = math.floor((80 - barWidth) / 2)
+  local barY = 13
+
+  gpu.setForeground(0xFFFFFF)
+  gpu.setBackground(0x000000)
+  gpu.fill(barX, barY, barWidth, barHeight, " ")
+
+  local progress = 0
+  while progress <= barWidth do
+    gpu.setForeground(0xFFFFFF)
+    gpu.setBackground(0xFFFFFF)
+    gpu.fill(barX, barY, progress, barHeight, " ")
+    gpu.setForeground(0x000000)
+    gpu.setBackground(0x000000)
+    gpu.fill(barX + progress, barY, 1, barHeight, " ")
+
+    os.sleep(0.05)
+    progress = progress + 1
+  end
+end
+
+gpu.setForeground(0xFFFFFF)
+gpu.setBackground(0x0000FF)
+gpu.fill(1, 1, 80, 25, " ")
+
+gpu.setBackground(0x0000FF)
+gpu.setForeground(0xFFFFFF)
+gpu.set(34, 12, "OpenPenguin")
+
+drawLoadingBar()
+
+dofile("SYS.lua")
