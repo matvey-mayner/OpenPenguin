bd001ab36fddb814e0a89bc6a9e150327596992b
matvey-mayner <144268527+matvey-mayner@users.noreply.github.com>
2024-06-10 17:40:58 +0300

    Update SYS32.lua

--- SYS32.lua
+++ SYS32.lua
@@ -31,26 +31,6 @@ local hotkeys = {
 
 gpu.setResolution(80,25)
 
-local module = {}
-
-local component = require("component")
-local gpu = component.gpu
-local event = require("event")
-
-local buttons = {}
-
-module.testButton = function(x, y)
-  for id, button in pairs(buttons) do
-      if x >= button.x1 and x <= button.x2 and y >= button.y1 and y <= button.y2 then
-          return id
-      end
-  end
-  return nil
-end
-
-module.buttonTouch = function(x, y, buttonName)
-  return module.testButton(x, y) == buttonName
-end
 
 function module.MakeButton(id, x, y, width, height, text, callback, foreground, background)
   gpu.setForeground(foreground)
@@ -63,6 +43,18 @@ function module.MakeButton(id, x, y, width, height, text, callback, foreground, 
   local buttonOnTouch = function(_, _, touchX, touchY, button, player)
     if module.buttonTouch(touchX, touchY, id) then
         callback(id, player, button)
+            module.testButton = function(x, y)
+  for id, button in pairs(buttons) do
+      if x >= button.x1 and x <= button.x2 and y >= button.y1 and y <= button.y2 then
+          return id
+      end
+  end
+  return nil
+end
+
+module.buttonTouch = function(x, y, buttonName)
+  return module.testButton(x, y) == buttonName
+end
     end
   end
   event.listen("touch", buttonOnTouch)
