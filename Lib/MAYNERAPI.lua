8543c85a55004e9fd81192927cd19c632e74c391
matvey-mayner <144268527+matvey-mayner@users.noreply.github.com>
2024-07-01 23:13:22 +0300

    Делаем кастомный апи и лекуем!!!

---
+++ Lib/MAYNERAPI.lua
@@ -0,0 +1,29 @@
+local gpu = component.gpu
+local screen = component.screen
+local component = require("component")
+local event = require("event")
+local computer = require("computer")
+
+local buttonW = 20
+local buttonH = 1
+
+local function isWithinButton(x, y, bx, by, bw, bh)
+    return x >= bx and x < bx + bw and y >= by and y < by + bh
+end
+
+function MAYNERAPI.DrawButton(x1, y1, width, height, text, foreground, background, callback)
+    gpu.setForeground(foreground)
+    gpu.setBackground(background)
+    gpu.fill(x1, y1, width, height, " ")
+    local textX = x1 + math.floor((width - #text) / 2)
+    local textY = y1 + math.floor(height / 2)
+    gpu.set(textX, textY, text)
+    
+    local function check(_, _, x2, y2)
+        if isWithinButton(x2, y2, x1, y1, width, height) then
+            callback()
+        end
+    end
+    
+    event.listen("touch", check)
+end
